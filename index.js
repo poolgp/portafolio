03947cd9e1abcf92b7d2157c193cafce2f08ef25
poolgp <145465364+poolgp@users.noreply.github.com>
2024-05-12 22:15:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll(".section");
 
-    sections.forEach((section) => {
-        section.addEventListener("click", function () {
-            section.classList.toggle("active");
-        });
-    });
-});
